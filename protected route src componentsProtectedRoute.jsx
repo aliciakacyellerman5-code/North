76a4cd58b17b5d201344e29d9967db1f3724ec4b
aliciakacyellerman5-code <aliciakacyellerman5@gmail.com>
2026-05-26 18:34:45 +0200

    Implement ProtectedRoute component for authentication

---
+++ protected route src componentsProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { auth } from "../firebase/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return user ? children : <Navigate to="/login" />;
+}
